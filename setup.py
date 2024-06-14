472a8c42309dc9ee85f1192a2b2f6e6b4bd1e6ca
BaimoQilin <admin@baimoqilin.top>
2024-06-14 18:24:05 +0800

    fix: Fix dependency missing problem

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ from cx_Freeze import setup, Executable
 import config
 
 files = [
+    "logs",
+    "codes",
     "config.yaml",
     "LICENSE",
     "README.md",
     "requirements.txt",
-    "logs"
-    # More files can be added here
+    "banner.jpeg"
 ]
 
-setup(name='CubeAgents', # Change this to the name of your app
+setup(name='BukkitGPT-v3',
       version=config.VERSION_NUMBER,
       maintainer="CubeGPT Team",
       maintainer_email="admin@cubegpt.org",
-      url="https://github.com/CubeGPT/CubeAgents",
+      url="https://github.com/CubeGPT/BukkitGPT-v3",
       license="Apache License 2.0",
-      description='A app-template for CubeGPT projects.',
+      description='An open source, free, AI-powered Minecraft Bukkit plugin generator',
       executables=[Executable('ui.py', base="gui")],
       options={
           "build_exe": {
